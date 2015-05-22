68da4b6b3a639dc3ab0c37c89c27293817ef676d
Zachary Alex Stern <zacharyalexstern@gmail.com>
2015-05-22 12:43:28 -0700

    Fundamentals initial commit.

---
+++ examples/vhost.pp
@@ -0,0 +1,4 @@
+include nginx
+
+nginx::vhost { 'punch.puppetlabs.vm': }
+nginx::vhost { 'judy.puppetlabs.vm': }
---
+++ manifests/init.pp
@@ -0,0 +1,39 @@
+class nginx (
+  $package = $nginx::params::package,
+  $owner = $nginx::params::owner,
+  $group = $nginx::params::Group,
+  $docroot = $nginx::params::docroot,
+  $confdir = $nginx::params::confdir,
+  $logdir = $nginx::params::logdir,
+  $user = $nginx::params::user,
+) inherits nginx::params {
+  File {
+    owner => $owner,
+    group => $group,
+    mode  => '0664',
+  }
+
+  package { $package:
+    ensure => present,
+  }
+
+  nginx::vhost { 'default':
+    docroot    => $docroot,
+    servername => $::fqdn,
+  }
+
+  file { "${docroot}/vhosts":
+    ensure => directory,
+  }
+
+  file { "${confdir}/nginx.conf":
+    ensure  => file,
+    content => template('nginx/nginx.conf.erb'),
+    notify  => Service['nginx']
+  }
+
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
